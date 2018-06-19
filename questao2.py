713e7f48cb5d4d51225b20cc1077e48cb87518f1
mariobarbosalima <40028716+mariobarbosalima@users.noreply.github.com>
2018-06-19 08:41:35 -0300

    Update questao2.py

--- questao2.py
+++ questao2.py
@@ -4,16 +4,15 @@ valor_total = 0
 if salario < 280:
     valor_total = ((salario * 0.2) + salario)
     print("O percentual de aumento foi de 20%")
-if salario > 280 and salario < 700:
+if salario > 280 and salario <= 700:
     valor_total = ((salario * 0.15) + salario)
     print("O percentual de aumento foi de 15%")
-if salario > 700 and salario < 1500:
+if salario > 700 and salario <= 1500:
     valor_total = (( salario * 0.1) + salario)
     print("O percentual de aumento foi de 10%")
 if salario > 1500 :
-    valor_total = ((salario * 0.5) + salario)
+    valor_total = ((salario * 0.05) + salario)
     print("O percentual de aumento foi de 5%")
-print("O salario apos o aumento e" ,valor_total,)
-print("O salario antes do reajuste e" ,salario,)
-print("O valor do aumento e de", valor_total - salario,)
-print("O novo salario vai ser" , valor_total)
+print("O salario apos o aumento vai ser R$" ,valor_total,)
+print("O salario antes do reajuste e R$" ,salario,)
+print("O valor do aumento e de R$", valor_total - salario,)
