f34efc8fbbf3047c42f52081327e4a69021e85f8
mariobarbosalima <40028716+mariobarbosalima@users.noreply.github.com>
2018-06-19 08:40:01 -0300

    Update questao1.py

--- questao1.py
+++ questao1.py
@@ -9,5 +9,21 @@ if num1 < num2 < num3:
     print("Ordem crescente:", num1, num2, num3)
     print("Ordem decrescente:", num3, num2, num1)
 if num1 > num2 < num3:
-    print("Ordem crescente:", num2, num3, num1)
-    print("Ordem decrescente:", num1, num3, num2)
+    print("Ordem crescente:", num2, num1, num3)
+    print("Ordem decrescente:", num3, num1, num2)
+if num1 < num2 > num3:
+    if num1 == 0:
+        print("Ordem crescente:", num1, num3, num2)
+        print("Ordem decrescente:", num2, num3, num1)
+if num1 < num2 > num3:
+    if num3 == 0:
+        print("Ordem crescente:", num3, num1, num2)
+        print("Ordem decrescente:", num2, num1, num3)
+if num1 < num2 > num3:
+    if num1 > num3 and num3 != 0:
+     print("Ordem crescente:", num3, num1, num2)
+     print("Ordem decrescente:", num2, num1, num3)
+if num1 < num2 > num3:
+    if num1 < num3:
+     print("Ordem crescente:", num1, num3, num2)
+     print("Ordem decrescente:", num2, num3, num1)
